b51d51b422127446eba214b27bd8f5f023557cdb
vamankumar00 <vamanokumar@gmail.com>
2023-05-26 16:29:05 +0500

    3rd

--- delete.php
+++ delete.php
@@ -2,8 +2,13 @@
 require "database.php";
 if (isset($_POST["submit"])) {
     $id = $_POST["id"];
+    $image = $_POST["image"];
     $sql = "DELETE FROM inventory_table WHERE id=$id;";
     $result = $conn->query($sql);
+    echo "$image";
+    die();
+    unlink($image);
+
     if ($conn->query($sql) === TRUE) {
         header("Location: index.php");
         die();
